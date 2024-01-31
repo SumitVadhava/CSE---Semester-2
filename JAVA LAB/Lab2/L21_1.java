1b4af45c003d16df232f39b5d4ccf633fe412c4d
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2024-01-31 19:43:23 +0530

    Delete JAVA LAB directory

--- JAVA LAB/Lab2/L21_1.java
+++
@@ -1,9 +0,0 @@
-public class L21_1 {
-        public static void main(String[] args){
-            double a = Double.parseDouble(args[0]);
-            double b = Double.parseDouble(args[1]);
-            double sum = a+b;
-            System.out.println("Sum:"+sum);
-        }
-   
-}
--- JAVA LAB/Lab2/L24.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class L24 {
-    public static void main(String[] args) {
-      Scanner sc= new Scanner(System.in);
-      System.out.println("Enter Radius=");
-     float r=sc.nextFloat();
-     System.out.println("Ans:"+(Math.PI*r*r));  
-    }
-}
